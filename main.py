c7ae8879e2dd34cbee4b53fb0f459f79436ff4a9
Abraham <fcoabrahamprz@gmail.com>
2024-05-21 08:32:35 -0700

    Change the examples used for the damage method

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from damage_calculator import DamageCalculator
 def main():
     damage_calculator = DamageCalculator()
     
-    print(damage_calculator.damage(40, 5, "second"))
-    print(damage_calculator.damage(100, 1, "minute"))
-    print(damage_calculator.damage(2, 100, "hour"))
+    print(damage_calculator.damage(40, 5, "seconds"))
+    print(damage_calculator.damage(100, 1, "mINUTE"))
+    print(damage_calculator.damage(2, 100, "horas"))
     
 
 if __name__ == "__main__":
